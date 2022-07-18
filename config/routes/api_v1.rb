88121a0328225d471dc9697e8c32b2249593c7fa
Krenar123 <krenardardhishta205@gmail.com>
2022-07-18 23:22:08 +0200

    Endpoints added

---
+++ config/routes/api_v1.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :pokemons, only: %i[index show]
+    end
+  end
+end
